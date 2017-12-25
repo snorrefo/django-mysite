7ea09b7a702c388909e8ff809ca95c63043811fe
Snorre Foss Westman <snorrefo@gmail.com>
2017-12-25 19:59:34 +0100

    Simple admin add Choice form. Not efficient

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -11,3 +11,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
