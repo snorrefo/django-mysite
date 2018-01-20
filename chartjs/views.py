6876f8802a5cae1b3bbabb449f251bef5d51a55f
Snorre Foss Westman <snorrefo@gmail.com>
2018-01-20 21:03:26 +0100

    Working example when distance from 2 BLE scanners are published to topics esp-link-03-r3 and esp-link-04-g4 every 3 secs

--- chartjs/views.py
+++ chartjs/views.py
@@ -29,50 +29,98 @@ def api_pos(request):
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
         # client.subscribe("happy-bubbles/ble/esp-link-03-r3/raw/170393f4dd7a")
-        client.subscribe("testsfw")
+        client.subscribe("happy-bubbles/presence/ha/#")
 
     # The callback for when a PUBLISH message is received from the server.
 
+    def on_message_esp_link_03(client, userdata, msg):
+        client.msgs["esp-link-03-r3"].nmessages += 1
+        client.msgs["esp-link-03-r3"].msg = msg
+        print("esp-link-03-r3: received message")
 
-    def on_message(client, userdata, msg):
-        # try:
-        # print(type(msg.payload))
-        # print("{} {}".format(msg.topic, msg.payload))
-        client.nmessages += 1
-        client.lastmsg = msg
-        print("received message")
-
-
-
-        # return {'x': msg.topic, 'y': msg.payload}
+    def on_message_esp_link_04(client, userdata, msg):
+        client.msgs["esp-link-04-g4"].nmessages += 1
+        client.msgs["esp-link-04-g4"].msg = msg
+        print("esp-link-04-g4: received message")
 
     client = mqtt.Client()
-    client.nmessages = 0
+
+    class Test(object):
+        def __init__(self):
+            self.msg = None
+            self.nmessages = 0
+
+    # msgs = {"esp-link-03-r3": Test()}
+    msgs = {"esp-link-03-r3": Test(), "esp-link-04-g4": Test()}
+    client.msgs = msgs
+
     client.on_connect = on_connect
-    client.on_message = on_message
+
+    client.message_callback_add("happy-bubbles/presence/ha/esp-link-03-r3",
+                                on_message_esp_link_03)
+
+    client.message_callback_add("happy-bubbles/presence/ha/esp-link-04-g4",
+                                on_message_esp_link_04)
+
+    # happy-bubbles/presence/ha/esp-link-03-r3
+    # {"id":"58b0f5296168","name":"iPhone","distance":45}
 
     client.connect("10.0.0.40", 1883, 2)
 
     while time.perf_counter() - t0 < 4:
-        if client.nmessages > 0:
+        n = 0
+        for key, msg in client.msgs.items():
+            if client.msgs[key].nmessages > 0:
+                n += 1
+        # print(n)
+        if n == len(client.msgs):
+            # All topics have received 1 or more messages
+            print("All topics have received 1 or more messages")
             break
+
         client.loop()
 
     print(time.perf_counter())
 
     client.disconnect()
 
-    if client.nmessages > 0:
-        print("message:")
-        print(client.lastmsg.payload)
-        msg = json.loads(client.lastmsg.payload)
-        x = msg['x']
-        y = msg['y']
-        print(x)
-        print(y)
-    else:
-        print("no message")
-        x = -1
-        y = -1
+    i = 1
+    x = []
+    y = []
+    for key, msg in client.msgs.items():
+        if client.msgs[key].nmessages > 0:
+            print("message:")
+            payload = client.msgs[key].msg.payload
+            print(payload)
+            msg = json.loads(payload)
+            x.append(i)
+            y.append(msg['distance'])
+            print(x)
+            print(y)
+
+        else:
+            print("no message:")
+            x.append(i)
+            y.append(-1)
+            print(x)
+            print(y)
+
+        i += 1
+
+    # if client.msgs["esp-link-03-r3"].nmessages > 0:
+    #     print("message:")
+    #     payload = client.msgs["esp-link-03-r3"].msg.payload
+    #     print(payload)
+    #     msg = json.loads(payload)
+    #     x = 3
+    #     y = msg['distance']
+    #     print(x)
+    #     print(y)
+    # else:
+    #     print("no message")
+    #     x = 3
+    #     y = -1
+    #     print(x)
+    #     print(y)
 
     return {'x': x, 'y': y}
